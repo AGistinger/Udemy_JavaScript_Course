2b50a4b3bae04869880fa2fd39f61e0cd539c5d2
Adrianne <54363707+AGistinger@users.noreply.github.com>
2020-11-30 17:09:32 -0500

    Started Developer skills section

---
+++ Developer_Skills/script.js
@@ -0,0 +1,41 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+/*
+const x = 23;
+
+if (x === 23) console.log(23);
+
+if (x !== 5) {
+  console.log("x is not equal to 5");
+} else {
+  console.log("x is equal to 5");
+}
+
+const calc_age = (birth_year) => 2037 - birth_year;
+
+console.log("xyz");
+
+console.log(calc_age(1991));
+console.log(2005);
+*/
+
+// Learning to Code
+// Process of coding, developer skills
+/*
+How to Fail....
+- Don't have a clear goal
+- Started courses and tutorials and would copy code without caring how it works
+- Didn't reinforce what was learned in the course by doing small challenges or taking notes
+- Dont' come up with his own project ideas
+
+How to Succeed....
+- Set a goal for yourself
+- Understand the code that you are typing
+- Take notes, try to challenge yourself
+- Practice on your own
+- Come up with your own project ideas
+- Don't get stuck of tutorial hell
+
+
+*/
