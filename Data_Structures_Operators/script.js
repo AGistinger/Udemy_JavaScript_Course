b521b79e4af6fa06f9b3899dd68ee1abe07f7a46
Adrianne <54363707+AGistinger@users.noreply.github.com>
2020-12-29 15:38:17 -0500

    Continued progress on data structures

--- Data_Structures_Operators/script.js
+++ Data_Structures_Operators/script.js
@@ -7,10 +7,6 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-  order: function (starter_index, main_index) {
-    return [this.starterMenu[starter_index], this.mainMenu[main_index]];
-  },
-
   openingHours: {
     thu: {
       open: 12,
@@ -24,6 +20,21 @@ const restaurant = {
       open: 0, // Open 24 hours
       close: 24,
     },
+  },
+
+  order: function (starter_index, main_index) {
+    return [this.starterMenu[starter_index], this.mainMenu[main_index]];
+  },
+
+  order_delivery: function ({
+    starter_index = 1,
+    main_index = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order recieved! ${this.starterMenu[starter_index]} and ${this.mainMenu[main_index]}, Will be delivered to ${address}, at ${time}`
+    );
   },
 };
 
@@ -65,4 +76,54 @@ console.log(p, q, r);
 
 /*
 ----------Destructuring Objects---------
+To destructure objects you use curly braces instead of brackets.
+You have to provide the variable names that exactly match the property names that match the object.
+Order of elements does not matter.
+*/
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// To specify new names for variables put a colon between a property name and a variable name
+const {
+  name: restaurant_name,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurant_name, hours, tags);
+
+// Set default values [] will set it to empty
+//  There is no property on the restaurant object called menu so you will see the empty array
+// Without the default values menu will show an empty array
+// This is helpful where you do not have data hard corded, like APIs
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables while destructuring objects
+let d = 111;
+let e = 999;
+const obj = { d: 23, e: 7, f: 14 };
+({ d, e } = obj); // syntax error, expects a code block, must wrap in parantheses
+
+// Nested objects
+const {
+  fri: { open: op, close: cl },
+} = openingHours;
+console.log(op, cl);
+
+// Order delivery function destructuring
+// function allows you to pass in an object of options
+restaurant.order_delivery({
+  time: "22:30",
+  address: "Vil del Sole, 21",
+  main_index: 2,
+  starter_index: 2,
+});
+
+restaurant.order_delivery({
+  address: "Vil del Sole, 21",
+  starter_index: 1,
+});
+
+/*
+-------------------The Spread Operator (...)--------------------
 */
