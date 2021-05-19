e9634c52adb5367ce7b69901a1e26ef2b7fde389
Adrianne <54363707+AGistinger@users.noreply.github.com>
2021-05-19 17:41:10 -0400

    completed project architecture chapter

--- Mapty_App/script.js
+++ Mapty_App/script.js
@@ -1,16 +1,5 @@
 "use strict";
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-const form = document.querySelector(".form");
-const containerWorkouts = document.querySelector(".workouts");
-const inputType = document.querySelector(".form__input--type");
-const inputDistance = document.querySelector(".form__input--distance");
-const inputDuration = document.querySelector(".form__input--duration");
-const inputCadence = document.querySelector(".form__input--cadence");
-const inputElevation = document.querySelector(".form__input--elevation");
-
 ////////////////////// How to Plan a Web Project //////////////////////
 /*
  * User Stories - 
@@ -60,6 +49,30 @@ const inputElevation = document.querySelector(".form__input--elevation");
    - Implementation of our plan using code
 */
 
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+/////////////// DOM ELEMENTS /////////////////
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+//////////////// GLOBAL VARIABLES ///////////////////
+let map;
+let mapEvent;
+
+////////////////// FUNCTIONS /////////////////////
+function clearInputs() {
+  inputDistance.value = "";
+  inputDuration.value = "";
+  inputCadence.value = "";
+  inputElevation.value = "";
+}
+
 ////////////////////// Using the GeoLocation API //////////////////////
 /*
  * Browser API similar to timers and internationalisation.
@@ -68,34 +81,75 @@ const inputElevation = document.querySelector(".form__input--elevation");
   - requires 2 functions, one for correctly getting geolocation and one for failing
  * 
  */
-
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      // Creates link on google maps
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-
       const coords = [latitude, longitude]; // array of coords used by leaflet
 
-      const map = L.map("map").setView(coords, 13); // creates map object
+      map = L.map("map").setView(coords, 13); // creates map object
 
+      // Creates map tiles
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      // handling clicks on map
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus(); // puts the field in focus after clicking on the map
+      });
+
+      // Creates link on google maps
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
     },
     function () {
       AudioListener("Could not get your position");
     }
   );
 }
+
+///////////////////////////// EVENT LISTENERS ////////////////////////////
+// Display the marker
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const { lat, lng } = mapEvent.latlng;
+
+  clearInputs();
+
+  // custom icon
+  let mapMarker = L.icon({
+    iconUrl: "icon.png",
+    iconSize: [50, 50],
+    iconAnchor: [22, 94],
+    popupAnchor: [0, -90],
+  });
+
+  // places marker where user clicks and sets the options for the popup
+  L.marker([lat, lng], { icon: mapMarker })
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: `running-popup`,
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
+
+// Change type of workout based on selected option (toggle)
+inputType.addEventListener("change", function () {
+  // Select the closest parent with the form row (DOM traversal) and toggle the hidden class
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
 
 ////////////////////// Displaying a Map Using Leaflet Library //////////////////////
 /*
@@ -105,4 +159,24 @@ if (navigator.geolocation) {
    integrated into the HTML to be used by the application.
  * Once Leaflet is active you can use the "L" namespace to access the functions for
    the API.
+ */
+
+//////////////////// Displaying a Map Marker Using Leaflet Library ////////////////////
+/*
+ * A event handler needs to be created to display a marker when the user clicks
+   on the map.
+  * A normal event handler will not work, there is a special one from the leaflet
+    library that is to be used.
+  * This needs to be called within the getCurrentPosition function for when a 
+    position is correctly obtained.
+  * map.on(event, function());  // object generated by leaflet
+     - Calls the click event with the special map functionality
+  * See leaflet documentation for popup options under UI layers
+ */
+
+//////////////////// Rendering Workout Input Form ////////////////////
+/*
+ * remove hidden class of the form from the style css.
+ * update the form based on what the workout type is selected by using
+   DOM traversal by selecting the nearest parent and then toggling the hidden class
  */
