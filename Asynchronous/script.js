605ad1272124f974237b90fd35e4fa89c158c924
Adrianne <agistinger@gmail.com>
2021-06-09 12:13:45 -0400

    Completed promises and fetch api chapter

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -43,14 +43,189 @@ const countriesContainer = document.querySelector(".countries");
 
 /////////////////////////////////////////////
 
-// XMLHttpRequest call (oldschool way)
-const request = new XMLHttpRequest();
+// function getCountryData(country) {
+//   // XMLHttpRequest call (oldschool way) stores result in a variable
+//   const request = new XMLHttpRequest();
 
-// Open a request
-request.open("GET", "https://restcountries.eu/rest/v2/name/prtugal");
+//   // Open a request ("type", "string of URL to where the AJAX call is made")
+//   request.open("GET", `https://restcountries.eu/rest/v2/name/${country}`);
 
-// Send a request (done in the background)
-request.send();
+//   // Send a request (done in the background)
+//   request.send();
 
-// Wait for the
-request.addEventListener("load", function () {});
+//   // Wait for the load event to execute code
+//   request.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText); // Convert JSON to JS object
+//     console.log(data);
+
+//     const html = `
+//     <article class="country">
+//           <img class="country__img" src="${data.flag}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               Number(data.population) / 1000000
+//             ).toFixed(1)} million</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${
+//               data.currencies[0].name
+//             }</p>
+//           </div>
+//         </article>
+//         `;
+
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// }
+
+// getCountryData("portugal");
+// getCountryData("usa");
+
+////////////////////////////////////////////////////////////////////
+//// Welcome to Callback Hell ///////
+
+// Having one callback function inside of another one (nested callbacks)
+function renderCountry(data, className = "") {
+  const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              Number(data.population) / 1000000
+            ).toFixed(1)} million</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>
+        `;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+}
+
+// function getCountryAndNeighbor(country) {
+//   // XMLHttpRequest call (oldschool way) stores result in a variable
+//   const request = new XMLHttpRequest();
+
+//   // Open a request ("type", "string of URL to where the AJAX call is made")
+//   request.open("GET", `https://restcountries.eu/rest/v2/name/${country}`);
+
+//   // Send a request (done in the background)
+//   request.send();
+
+//   // Wait for the load event to execute code
+//   request.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText); // Convert JSON to JS object
+//     console.log(data);
+
+//     // Render Country Main
+//     renderCountry(data);
+
+//     // Get Neighbor Country
+//     const neighbor = data.borders[0];
+
+//     if (!neighbor) return;
+
+//     const requestNeighbor = new XMLHttpRequest();
+
+//     // Open a request ("type", "string of URL to where the AJAX call is made")
+//     requestNeighbor.open(
+//       "GET",
+//       `https://restcountries.eu/rest/v2/alpha/${neighbor}`
+//     );
+
+//     // Send a request (done in the background)
+//     requestNeighbor.send();
+
+//     requestNeighbor.addEventListener("load", function () {
+//       const data2 = JSON.parse(this.responseText); // Country codes do not return an array
+//       renderCountry(data2, "neighbour");
+//     });
+//   });
+// }
+
+// getCountryAndNeighbor("usa");
+
+// Callback hell example
+// Callback hell can be easily identified by the triangle shape of the code
+// Makes code look messy, hard to maintain and understand
+// setTimeout(() => {
+//   console.log("1 second passed");
+//   setTimeout(() => {
+//     console.log("2 seconds passed");
+//     setTimeout(() => {
+//       console.log("3 seconds passed");
+//       setTimeout(() => {
+//         console.log("4 seconds passed");
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+////////////////////////////////////////////////////////////////////
+//// Promises and the Fetch API ///////
+
+// Fetch function you can pass in the URL and options (returns a promise)
+const request = fetch(`https://restcountries.eu/rest/v2/name/portugal`); // promise
+
+/*
+ What is a Promise? (ES6 / 2015)
+  - An object that is used as a placeholder for the future result of an
+    asynchronous operation.
+  - A container for an asynchrounously delivered value.
+  - A container for a future value. (ex. response coming from AJAX call)
+  - Example) Promise that I will receive money if I guess the correct outcome (lottery ticket)
+    * I buy lottery ticket (promise) right now
+    * Lottery draw happens asynchronously
+    * If correct outcome, I receive money, because it was promised
+  - By using Promises we no longer need to rely on events and callbacks passed
+    asynchronous functions to handler asynchronous results.
+  - Instead of nesting callbacks, we can chain promises for a sequence of
+    asynchronous operations: escaping callback hell.
+  - Promises are only fullfilled once
+
+  The Promise Lifecycle:
+   - Pending (before the future value is available)\
+     - Async task running
+   - Settled (asynchronous task has finished)
+     - Fullfilled (successfully got the data)
+     - Rejected (an error happened)
+
+   - Build promise, (fetch API returns promise)
+   - Consume promise, (promise was settled ex) data returned)
+*/
+
+////////////////////////////////////////////////////////////////////
+//// Consuming Promises ///////
+
+// Then method will execute a callback method as soon as the promise is fullfilled
+// function getCountryData(country) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(function (response) {
+//       // json() is available on all response objects will also return promise
+//       // that promise will also need a then method
+//       // callback functions can be arrow functions
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       renderCountry(data[0]);
+//     });
+// }
+
+// getCountryData("portugal");
+
+////////////////////////////////////////////////////////////////////
+//// Chaining Promises ///////
+function getCountryData(country) {
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0]));
+}
+
+getCountryData("portugal");
