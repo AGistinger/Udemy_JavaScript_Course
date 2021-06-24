35f5b06f1513397ffbf5cc6709eca9210208fad3
Adrianne <agistinger@gmail.com>
2021-06-23 20:26:24 -0400

    Completed challenge #1

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -249,37 +249,148 @@ const request = fetch(`https://restcountries.eu/rest/v2/name/portugal`); // prom
   Besides then and catch there is a finally() method, used for something
   that always needs to happen, no matter what the result of the promise.
   ex) hide a loading spinner.
+
+  catch can use a err object.  The error object has a message property that
+  can be used to show the message to the user.
+
+  The finally method will be called whether the promise was fullfilled or
+  rejected.  Used for something that always needs to happen no matter the 
+  result of the promise. (not always useful)
 */
 function renderError(msg) {
   countriesContainer.insertAdjacentText("beforeend", msg);
   // countriesContainer.style.opacity = 1;
 }
 
+function getJSON(url, errorMsg = "Something went wrong") {
+  return fetch(url).then((response) => {
+    if (!response.ok) {
+      throw new Error(`${errorMsg} (${response.status})`);
+    }
+    return response.json();
+  });
+}
+
 function getCountryData(country) {
   // Country
-  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-    .then((response) => response.json())
+  getJSON(
+    `https://restcountries.eu/rest/v2/name/${country}`,
+    "Country not found"
+  )
     .then((data) => {
       renderCountry(data[0]);
       const neighbor = data[0].borders[0];
-      if (!neighbor) return;
+      if (!neighbor) throw new Error("No neighbour found!");
 
       // Countries neighbor (returns promise)
-      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbor}`);
+      return getJSON(
+        `https://restcountries.eu/rest/v2/alpha/${neighbor}`,
+        "Country not found"
+      );
     })
-    .then((response) => response.json())
     .then((data) => renderCountry(data, "neighbour"))
     .catch((err) => {
-      console.error(`${err} 🔥🔥🔥🔥🔥`);
-      renderError(`Something went wrong 🔥🔥🔥 ${err.message}. Try again!`);
+      console.error(`${err}`);
+      renderError(`${err.message}`);
     })
     .finally(() => {
       countriesContainer.style.opacity = 1;
     });
 }
 
-btn.addEventListener("click", function () {
-  getCountryData("portugal");
-});
+// btn.addEventListener("click", function () {
+//   getCountryData("portugal");
+// });
 
-getCountryData("asldkjfas");
+// getCountryData("australia"); // Will get error
+
+////////////////////////////////////////////////////////////////////
+//// Throwing Errors Manually ///////
+/*
+You can throw a custom error by writing
+  throw new Error(`Text`);
+
+Throw will cause promises to immediately reject.
+
+It is important to handle all possible errors for the user.
+*/
+
+////////////////////////////////////////////////////////////////////
+//// Coding Challenge #1 ///////
+/*
+In this hcallenge you will build a function "whereAmI" which renders a 
+country ONLY based on GPS coordinates.  For that, you will use a 
+second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function "whereAmI" which takes as inputs a latitude value
+(lat) and a longitude value (lng), (these are GPS coordinates, examples are below).
+2. Do "reverse geocoding" of the provided coordinates.  Reverse geocoding
+means to convert coordinates to a meaningful location, like a city and
+country name.  Use this API to do reverse geocoding:
+https://geocode.xyz/api
+The AJAX call will be done to a URL with this format:
+https://geocode.xyz/52.508, 13.381?geoit=json
+Use the fetch API and the promises to get the data.  Do NOT use the getJSON
+function we created, that is cheating.
+3. Once you have the data, take a look at it in the console to see all the 
+attributes that you recieved about the provided location.  Then, using 
+this data, log a message like this to the console: `Your are in Berlin, Germany`.
+4. Chain a .catch method to the end of the promise chain and log errors to 
+the console.
+4. This API allows you to make only 3 requests per second.  If you reload fast, you 
+will get this error with code 403.  This is an error with the request.
+Remember, fetch() does NOT reject the promise in this case.  So create an error
+to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the recieved dtata to render a country.  So take the
+revelant attribute from the geocoding API result, and plug it into the 
+countries API that we have been using.
+7.  Render the country and catch any errors, just like we have done in the last
+lecture (you can even copy this code, no need to type the same code).
+
+TEST COORDINATES 1: 52.508, 13.381 (latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 3: -33.933, 18.474
+*/
+function getLocation() {
+  const position = navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude.toFixed(3);
+    const lng = position.coords.longitude.toFixed(3);
+
+    whereAmI(lat, lng);
+  });
+}
+
+function renderFailure(msg) {
+  countriesContainer.insertAdjacentHTML("beforeend", msg);
+}
+
+function getAPIdata(url, errmsg = "Failed to obtain API data") {
+  return fetch(url).then((response) => {
+    // response
+    if (!response.ok) {
+      throw new Error(`${errmsg} (${response.status})`);
+    }
+    // return data
+    return response.json();
+  });
+}
+
+function whereAmI(lat, lng) {
+  getAPIdata(`https://geocode.xyz/${lat}, ${lng}?geoit=json`)
+    .then((data) => {
+      console.log(`You are in ${data.city}, ${data.country}`);
+      getCountryData(data.country); // use restcountries api to change coords into country
+    })
+    .catch((err) => {
+      console.error(err);
+      renderFailure(`${err.message}`);
+    });
+}
+
+// btn.addEventListener("click", whereAmI(lat, lng));
+btn.addEventListener("click", getLocation);
